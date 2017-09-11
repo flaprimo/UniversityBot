2aaad15160dd09949677a258d4047acaac1d05b1
flaprimo <fla.primo.engineer@outlook.com>
2017-09-11 18:31:10 +0200

    removed pycache and mo_po generator

--- locale/update_po_mo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-#msginit --locale=it --output=it/LC_MESSAGES/messages.po --input=messages.pot
-#msgmerge --update --no-fuzzy-matching --backup=off it/LC_MESSAGES/messages.po messages.pot
-msgfmt it/LC_MESSAGES/it.po --output-file it/LC_MESSAGES/it.mo
