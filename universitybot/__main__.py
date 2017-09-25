7ba5caa954be03881aba66d51eba33118308be76
flaprimo <fla.primo.engineer@outlook.com>
2017-09-26 00:18:16 +0200

    modified gitignore

--- universitybot/__main__.py
+++ universitybot/__main__.py
@@ -12,6 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+
 def main():
     # Load configuration file
     with open('conf/conf.test.json') as json_data_file:
