63eb110be5aa7a56d066f057c071bb6a35c5b16c
Paolo Paterna <paolo.paterna@gmail.com>
2018-07-27 14:50:49 +0200

    Now travis.yml is correct

--- docker_install.sh
+++ docker_install.sh
@@ -2,4 +2,4 @@
 
 git pull
 docker build -t universitybot .
-docker run --name=universitybot -v /opt/UniversityBot/logs:/opt/UniversityBot/logs -v /opt/UniversityBot/cache:/opt/UniversityBot/cache -v /opt/UniversityBot/config:/opt/UniversityBot/config --restart=on-failure:10 universitybot
+docker run -d --name=universitybot -v /opt/UniversityBot/logs:/opt/UniversityBot/logs -v /opt/UniversityBot/cache:/opt/UniversityBot/cache -v /opt/UniversityBot/config:/opt/UniversityBot/config --restart=on-failure:10 universitybot
